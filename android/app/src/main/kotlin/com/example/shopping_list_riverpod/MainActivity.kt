c79972f743c3ff4486580e79727b80ed2cfd159a
Hazuli Fidastian <hazulifidastian@gmail.com>
2021-05-13 00:58:58 +0700

    Initial commit     - Firebase     - Auth repository     - Auth controller     - Test anonymous and logout

---
+++ android/app/src/main/kotlin/com/example/shopping_list_riverpod/MainActivity.kt
@@ -0,0 +1,6 @@
+package id.hazulifidastian.shopping_list_riverpod
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:shopping_list_riverpod/controllers/auth_controller.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends HookWidget {
+  @override
+  Widget build(BuildContext context) {
+    final authControllerState = useProvider(authControllerProvider.state);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Shopping List'),
+        leading: authControllerState != null
+            ? IconButton(
+                icon: const Icon(Icons.logout),
+                onPressed: () => context.read(authControllerProvider).signOut())
+            : null,
+      ),
+    );
+  }
+}
