9789a72459363637d3df6bf29b23554794e6ecad
Hazuli Fidastian <hazulifidastian@gmail.com>
2021-05-13 22:50:37 +0700

    Add item dialog

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:shopping_list_riverpod/controllers/auth_controller.dart';
+import 'package:shopping_list_riverpod/controllers/item_list_controller.dart';
+import 'package:shopping_list_riverpod/models/item_model.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -36,6 +38,70 @@ class HomeScreen extends HookWidget {
                 icon: const Icon(Icons.logout),
                 onPressed: () => context.read(authControllerProvider).signOut())
             : null,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => AddItemDialog.show(context, Item.empty()),
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class AddItemDialog extends HookWidget {
+  static void show(BuildContext context, Item item) {
+    showDialog(
+      context: context,
+      builder: (context) => AddItemDialog(item: item),
+    );
+  }
+
+  final Item item;
+
+  const AddItemDialog({Key? key, required this.item}) : super(key: key);
+
+  bool get isUpdating => item.id != null;
+
+  @override
+  Widget build(BuildContext context) {
+    final textController = useTextEditingController(text: item.name);
+    return Dialog(
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              controller: textController,
+              autofocus: true,
+              decoration: const InputDecoration(hintText: 'Item name'),
+            ),
+            const SizedBox(height: 12.0),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: isUpdating
+                      ? Colors.orange
+                      : Theme.of(context).primaryColor,
+                ),
+                onPressed: () {
+                  isUpdating
+                      ? context.read(itemListControllerProvider).updateItem(
+                            updatedItem: item.copyWith(
+                              name: textController.text.trim(),
+                              obtained: item.obtained,
+                            ),
+                          )
+                      : context
+                          .read(itemListControllerProvider)
+                          .addItem(name: textController.text.trim());
+                  Navigator.of(context).pop();
+                },
+                child: Text(isUpdating ? 'Update' : 'Add'),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
